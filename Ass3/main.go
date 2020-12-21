f076286844374b1a41258b2c3bd0c18bfe0a4c45
Mark Standen <mark@markstanden.uk>
2020-12-21 21:46:11 +0000

    Ass 3 complete

--- Ass3/main.go
+++ Ass3/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
-	
+	/* 	Get the filename from the commandline argument provided,
+	assign to fn assume filename is the last provided argument. */
+	fn := os.Args[len(os.Args)-1]
+
+	/* 
+	Open the file, check for errors in loading file
+	 */
+	f, err := os.Open(fn)
+	if err != nil {
+		fmt.Println("--- Issues loading file ---")
+	} else {
+		/* If the file loads OK, copy contents to the OS's stdout */
+		fmt.Printf("--- Reading file : %s ---\n\n", fn)
+		io.Copy(os.Stdout, f)
+		fmt.Println("\n\n--- File read complete! ---")
+	}
 }
